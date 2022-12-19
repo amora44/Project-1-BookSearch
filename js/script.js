5dd5484d2783762aab7fdec5e4ef713e32f0535a
Alyssa Mora <alyssaa4@gmail.com>
2022-12-19 08:51:35 -0600

    api displays data

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
 $.ajax('https://www.googleapis.com/books/v1/volumes?q=key=AIzaSyAvrrPM1i1rCx_HHEYcXZqOwBuZsimUhLo')
+    
+function render(data) {
+    $("results").empty();
+    
+    $.each(data.items, function(i, item) {
+        var title= item.volumeInfo.title;
+        var authors = item.volumeInfo.authors;
+        var categories = item.volumeInfo.categories;
+        var coverImageUrl = item.volumeInfo.imageLinks.thumbnail;
+        var width = item.volumeInfo.imageLinks.width;
+        var height = item.volumeInfo.imageLinks.height;
+            
+            
+        $("#results").append(
+            "<img src='" + coverImageUrl + "' style='width: " + width + "px; height: " + height + "px;'>" +
+            "<h2>" + title + "</h2>" +
+            "<p>By " + authors + "</p>" +
+            "<p>Categories: " + categories + "</p>" 
+        );
+    });
+}   
+$("#form").submit(function(e) {
+     e.preventDefault();
 
+     var searchBook = $("#search-book").val();
 
-function searchBooks() {
-    var searchTerm = $("#search").val();
     $.ajax({
-      url: "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm,
+      url: "https://www.googleapis.com/books/v1/volumes?q=" + searchBook,
       type: "GET",
-      success: function(results) {
-        displayResults(results);
+      success: function(data) {
+        render(data);
       }
     });
-  }
+  });
 
+  $("#clear-btn").click(function() {
+    $("#results").empty();
+  });
 
 
 
